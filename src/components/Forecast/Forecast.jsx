b1b14948babf4be1dc0663452cfb2be65d22e4ee
Salahuddin Hissam <salahuddin.hissamuddin@gmail.com>
2021-11-17 00:59:52 +0100

    m

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -12,14 +12,14 @@ import styles from './Forecast.module.css';
 const Forecast = ({ forecast }) => (
     <Container className={styles.box}>
         <Row>
+            <Col xs={12} md={8} className="d-flex flex-column justify-content-between">
+                <UpcomingDaysForecast days={forecast.upcomingDays} />
+                <CurrentDayDescription forecast={forecast.currentDayDetails} />
+            </Col>
             <Col xs={12} md={4}>
                 <div className={styles.card}>
                     <CurrentDay {...forecast.currentDay} />
                 </div>
-            </Col>
-            <Col xs={12} md={8} className="d-flex flex-column justify-content-between">
-                <CurrentDayDescription forecast={forecast.currentDayDetails} />
-                <UpcomingDaysForecast days={forecast.upcomingDays} />
             </Col>
         </Row>
     </Container>
